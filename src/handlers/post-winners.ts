278899cb77a5489dc79d3fc15071aebd88f5c935
Gerardo Leal <gleal@atlassian.com>
2021-07-15 22:43:06 +1000

    Post to get a number of winners done

--- src/handlers/post-winners.ts
+++ src/handlers/post-winners.ts
@@ -1,9 +1,54 @@
-export const handler = async (event: any, context: any) => {
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from "aws-lambda";
+
+import { RawChallenge, Challenge } from "../types";
+
+const getChallenge = (event: APIGatewayProxyEvent): Challenge | null => {
+  if (!event.body) {
+    return null;
+  }
+
+  try {
+    const rawChallenge: RawChallenge = JSON.parse(event.body);
+    return {
+      ...rawChallenge,
+      students: rawChallenge.students.split(","),
+    };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const getRandomWinners = (challenge: Challenge) =>
+  challenge.students
+    .sort(() => 0.5 - Math.random())
+    .slice(0, challenge.winners);
+
+export const handler: APIGatewayProxyHandler = async (event) => {
   console.log(event);
+
+  const challenge = getChallenge(event);
+
+  if (!challenge) {
+    return {
+      statusCode: 400,
+      body: "Invalid request",
+    };
+  }
+
+  if (challenge.students.length === 0) {
+    return {
+      statusCode: 400,
+      body: "Invalid request: no students given",
+    };
+  }
+
+  const winners = getRandomWinners(challenge);
+
+  console.log(winners);
 
   return {
     statusCode: 200,
-    headers: {},
-    body: JSON.stringify({ result: "heeeey" }),
+    body: JSON.stringify({ winners }),
   };
 };
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface RawChallenge {
+  challengeID: number;
+  students: string;
+  winners: number;
+}
+
+export interface Challenge extends Omit<RawChallenge, "students"> {
+  students: string[];
+}
