fa64183e8317384b7d202cd6ef4123c84f489ecd
Gerardo Leal <gleal@atlassian.com>
2021-07-15 23:14:40 +1000

    Get the list of challenges

--- src/handlers/get-winners.ts
+++ src/handlers/get-winners.ts
@@ -1,9 +1,31 @@
-export const handler = async (event: any, context: any) => {
+import { APIGatewayProxyHandler } from "aws-lambda";
+import * as AWS from "aws-sdk";
+
+const getWinners = async () => {
+  const db = new AWS.DynamoDB.DocumentClient();
+
+  const TABLE_NAME = process.env.TABLE_NAME || "";
+
+  const params = {
+    TableName: TABLE_NAME,
+  };
+
+  return await db.scan(params).promise();
+};
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
   console.log(event);
 
-  return {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify({ result: "heeeey" }),
-  };
+  try {
+    const challenges = await getWinners();
+
+    console.log(challenges);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(challenges),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify(error) };
+  }
 };
